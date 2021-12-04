cb0a0321ed0f65dfa6ae3c1e840e6abe9ee88321
ParulARMY <87768125+ParulARMY@users.noreply.github.com>
2021-12-04 10:42:19 +0530

    Delete Exercise04.py

--- Exercise04.py
+++
@@ -1,6 +0,0 @@
-# swaping of two numbers.
-a = input("Enter the value of a")
-b = input("Enter the value of b")
-print("value of a before swaping is:",a+" value of b before swaping is:",b )
-a,b=b,a
-print("value of a after swaping is:",a+" value of b after swaping is:",b)
